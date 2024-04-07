3acdb46f26a363441aa06359b1ca81745a495a92
sbnoman01 <sbnoman27@gmail.com>
2024-04-07 16:36:54 +0600

    publish

---
+++ js/script.js
@@ -0,0 +1,59 @@
+(function($) {
+	
+	"use strict";
+	
+	
+	//Add OnepageNav / Sidebar
+	function sideNav() {
+		if($('.menu-box .sticky-menu').length){
+			$('.menu-box .sticky-menu ul').onePageNav();
+		}
+	}
+	
+	//Add Scroll Bar To Sidebar
+	if($('#sidebar .menu-box').length){
+		$("#sidebar .menu-box").mCustomScrollbar({
+			axis:"y",
+			autoExpandScrollbar:false
+		});
+	}
+	
+	//animate to top on Page Refresh
+    $('html, body').animate({
+	   scrollTop: $('html, body').offset().top
+	}, 1000);
+	
+	//Accordion Box
+	if($('.accordion-box').length){
+		$(".accordion-box").on('click', '.acc-btn', function() {
+			
+			var outerBox = $(this).parents('.accordion-box');
+			var target = $(this).parents('.accordion');
+			
+			if($(this).hasClass('active')!==true){
+				$(outerBox).find('.accordion .acc-btn').removeClass('active');
+			}
+			
+			if ($(this).next('.acc-content').is(':visible')){
+				return false;
+			}else{
+				$(this).addClass('active');
+				$(outerBox).children('.accordion').removeClass('active-block');
+				$(outerBox).find('.accordion').children('.acc-content').slideUp(300);
+				target.addClass('active-block');
+				$(this).next('.acc-content').slideDown(300);	
+			}
+		});	
+	}
+
+
+/* ==========================================================================
+   When document is ready, do
+   ========================================================================== */
+   
+	$(document).on('ready', function() {
+		sideNav();
+	});
+
+
+})(window.jQuery);
